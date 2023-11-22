144264c8c613940804f4c6a50692f9a0a8245c8d
anas <ansarianas3115@gmail.com>
2023-11-22 18:31:04 +0530

    Updated code

--- Model/Rewrite/Quote/Address/Rate.php
+++ Model/Rewrite/Quote/Address/Rate.php
@@ -13,6 +13,7 @@ use Magento\Quote\Model\Quote\Address\RateResult\AbstractResult;
 use Magento\Quote\Model\Quote\Address\RateResult\Error;
 use Magento\Quote\Model\Quote\Address\RateResult\Method;
 use Magento\Checkout\Model\Session as CheckoutSession;
+use Zend_Log_Exception;
 
 class Rate extends \Magento\Quote\Model\Quote\Address\Rate
 {
@@ -43,7 +44,6 @@ class Rate extends \Magento\Quote\Model\Quote\Address\Rate
         Registry $registry,
         AbstractResource $resource = null,
         AbstractDb $resourceCollection = null,
-
         array $data = []
     ) {
         $this->scopeConfig = $scopeConfig;
@@ -56,7 +56,7 @@ class Rate extends \Magento\Quote\Model\Quote\Address\Rate
             $data
         );
     }
-    
+
     /**
      * @param AbstractResult $rate
      * @return $this|Rate
@@ -70,9 +70,25 @@ class Rate extends \Magento\Quote\Model\Quote\Address\Rate
         $textToMatch = $this->scopeConfig->getValue('Codilar_ShippingAddress/products_settings/text');
         $finalPrice = $rate->getPrice();
 
-// Check if the shipping address matches the specified text to add extra shipping cost
+        $shippingMethodCode = $rate->getMethod();
+
+        // Check if the shipping method is Flat Rate and the address contains the specified text
+        $isFlatRateMethod = strtolower($shippingMethodCode) === 'flatrate';
+        $addressContainsText = false;
+
         $shippingAddress = $this->checkoutSession->getQuote()->getShippingAddress();
-        if ($shippingAddress->getStreetLine(1) && str_contains($shippingAddress->getStreetLine(1), $textToMatch)) {
+        $streetLines = $shippingAddress->getStreet();
+
+        // Check if the address lines contain the specified text
+        foreach ($streetLines as $streetLine) {
+            if (str_contains($streetLine, $textToMatch)) {
+                $addressContainsText = true;
+                break;
+            }
+        }
+
+        // Apply the extra cost only if it's Flat Rate and the address contains the text
+        if ($isFlatRateMethod && $addressContainsText) {
             $finalPrice += $additionalPrice;
         }
 
